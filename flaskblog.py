687fdde471247261ac8002922bbe2383cc01306f
Konrad <konrad.janiak@gmail.com>
2021-01-06 20:55:20 +0100

    After Part 2

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        "author": "Jan Kowalski",
+        "title": "Blog post 1",
+        "content": "First post content",
+        "date_posted": "1 stycznia 2021"
+    },
+    {
+        "author": "Maria Kowalska",
+        "title": "Blog post 2",
+        "content": "Seconds post content",
+        "date_posted": "4 stycznia 2021"
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template("about.html", title="About")
 
 if __name__ == "__main__":
     app.run(debug=True)
